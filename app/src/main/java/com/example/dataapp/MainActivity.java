73cffa3dffa551b3af5cd423d64f2bde0f6332c7
abdul <abdul.tanveer@gmail.com>
2019-07-10 06:41:29 +0530

    calllog content provider

--- app/src/main/java/com/example/dataapp/MainActivity.java
+++ app/src/main/java/com/example/dataapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dataapp;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.CallLog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -16,14 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ContentResolver contentResolver = getContentResolver();
         Uri uriSms = Uri.parse("content://sms/inbox");  //TABLE_NAME
+        Uri allCalls = Uri.parse("content://call_log/calls");
 
-        Cursor dataCursor = contentResolver.query(uriSms,
+        Cursor dataCursor = contentResolver.query(allCalls,
                 null,null,null,null);
        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
                 android.R.layout.simple_list_item_1,
                // R.layout.my_list_item,
                 dataCursor,
-                new String[]{"body"},
+                new String[]{CallLog.Calls.NUMBER},
                 //new int[]{R.id.textViewTitle,R.id.textViewSubtitle},
                new int[]{android.R.id.text1},
                0 );
