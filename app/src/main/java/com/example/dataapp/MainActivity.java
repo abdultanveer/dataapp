e893b86f3faf7194f2db04a8f2fd6aeade1c6f01
abdul <abdul.tanveer@gmail.com>
2019-07-10 07:07:10 +0530

    contacts content provider

--- app/src/main/java/com/example/dataapp/MainActivity.java
+++ app/src/main/java/com/example/dataapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -18,16 +19,17 @@ public class MainActivity extends AppCompatActivity {
         ContentResolver contentResolver = getContentResolver();
         Uri uriSms = Uri.parse("content://sms/inbox");  //TABLE_NAME
         Uri allCalls = Uri.parse("content://call_log/calls");
+        Uri contacts = ContactsContract.Contacts.CONTENT_URI;
 
-        Cursor dataCursor = contentResolver.query(allCalls,
+        Cursor dataCursor = contentResolver.query(contacts,
                 null,null,null,null);
        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1,
+                android.R.layout.simple_list_item_2,
                // R.layout.my_list_item,
                 dataCursor,
-                new String[]{CallLog.Calls.NUMBER},
+                new String[]{ContactsContract.Contacts.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER},
                 //new int[]{R.id.textViewTitle,R.id.textViewSubtitle},
-               new int[]{android.R.id.text1},
+               new int[]{android.R.id.text1,android.R.id.text2},
                0 );
 
         ListView dataListView =  findViewById(R.id.cpListview);
