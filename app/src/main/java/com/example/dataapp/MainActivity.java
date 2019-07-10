d68ad027fcd2cab4a700623712b033e70ffbbf05
abdul <abdul.tanveer@gmail.com>
2019-07-10 08:23:53 +0530

    loaders

--- app/src/main/java/com/example/dataapp/MainActivity.java
+++ app/src/main/java/com/example/dataapp/MainActivity.java
@@ -5,35 +5,66 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.CallLog;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    public static int COOLIE_ID = 007;
+    Bundle specialInstructions;
+    SimpleCursorAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ContentResolver contentResolver = getContentResolver();
-        Uri uriSms = Uri.parse("content://sms/inbox");  //TABLE_NAME
+        LoaderManager loaderManager = getSupportLoaderManager();
+        loaderManager.initLoader(COOLIE_ID,specialInstructions,this);
+
+        /*ContentResolver contentResolver = getContentResolver();
         Uri allCalls = Uri.parse("content://call_log/calls");
         Uri contacts = ContactsContract.Contacts.CONTENT_URI;
 
-        Cursor dataCursor = contentResolver.query(contacts,
-                null,null,null,null);
-       SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_2,
+        Cursor dataCursor = contentResolver.query(contacts, //querying a db on main thread
+                null,null,null,null);*/
+        adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
                // R.layout.my_list_item,
-                dataCursor,
-                new String[]{ContactsContract.Contacts.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER},
+                null,
+                new String[]{"body"},
                 //new int[]{R.id.textViewTitle,R.id.textViewSubtitle},
-               new int[]{android.R.id.text1,android.R.id.text2},
+               new int[]{android.R.id.text1},
                0 );
 
         ListView dataListView =  findViewById(R.id.cpListview);
         dataListView.setAdapter(adapter);
+
+    }
+
+    @NonNull
+    @Override    //when you hire a coolie -- you give him some instructions -- like from which warehouse[db] he has to get the goods
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle instructions) {
+        if(id == COOLIE_ID){
+            Uri uriSms = Uri.parse("content://sms/inbox");  //TABLE_NAME
+
+            return new CursorLoader(this,uriSms,null,null,null,null);
+        }
+        return null;
+    }
+
+    // coolie is done with his job  -- where to put the goods
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor dataCursor) {
+            adapter.swapCursor(dataCursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
     }
 }
